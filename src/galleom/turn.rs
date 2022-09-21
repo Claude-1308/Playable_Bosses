1292038f78c892d9919b60ebf3c584cd13bf2d09
ClaudevonRiegan <63413745+ClaudevonRiegan@users.noreply.github.com>
2022-09-21 12:43:51 +0530

    Update turn.rs

--- src/galleom/turn.rs
+++ src/galleom/turn.rs
@@ -18,52 +18,24 @@ use crate::common::modules::*;
 pub unsafe fn turn_coroutine(item: &mut L2CAgentBase) -> L2CValue {
     let lua_state = item.lua_state_agent;
     let module_accessor = sv_system::battle_object_module_accessor(lua_state);
-    MotionModule::change_motion(module_accessor,Hash40::new("tail"),0.0,1.0,false,0.0,false,false);
-    boss_private::main_energy_from_param(lua_state,ItemKind(*ITEM_KIND_LIOLEUSBOSS),Hash40::new("energy_param_turn"),0.0);
+    MotionModule::change_motion(module_accessor,Hash40::new("turn"),0.0,1.0,false,0.0,false,false);
+    boss_private::main_energy_from_param(lua_state,ItemKind(*ITEM_KIND_GALLEOM),Hash40::new("energy_param_turn"),0.0);
+    boss_private::sub1_energy_from_param_inherit_all(lua_state,ItemKind(*ITEM_KIND_GALLEOM),Hash40::new("energy_param_turn_brake"));
     return L2CValue::I32(0)
 }
 
 pub unsafe fn turn_status(item: &mut L2CAgentBase) -> L2CValue {
     let lua_state = item.lua_state_agent;
     let module_accessor = sv_system::battle_object_module_accessor(lua_state);
-    let owner = BossModule::get_owner(module_accessor);
     if MotionModule::is_end(module_accessor) {
-        WorkModule::set_int(owner,*SITUATION_KIND_GROUND,FIGHTER_MARIO_INSTANCE_WORK_ID_INT_BOSS_SITUATION);
-        PostureModule::update_rot_y_lr(module_accessor);
-        StatusModule::change_status_request(module_accessor,*ITEM_LIOLEUSBOSS_STATUS_KIND_WAIT,false);
-    }
-    return L2CValue::I32(0)
-}
-
-pub unsafe fn turn_air_coroutine(item: &mut L2CAgentBase) -> L2CValue {
-    let lua_state = item.lua_state_agent;
-    let module_accessor = sv_system::battle_object_module_accessor(lua_state);
-    MotionModule::change_motion(module_accessor,Hash40::new("turn_air"),0.0,0.6,false,0.0,false,false);
-    boss_private::main_energy_from_param(lua_state,ItemKind(*ITEM_KIND_LIOLEUSBOSS),Hash40::new("energy_param_fly_turn"),0.0);
-    boss_private::sub1_energy_from_param_inherit(lua_state,ItemKind(*ITEM_KIND_LIOLEUSBOSS),Hash40::new("energy_param_fly_brake"));
-    return L2CValue::I32(0)
-}
-
-pub unsafe fn turn_air_status(item: &mut L2CAgentBase) -> L2CValue {
-    let lua_state = item.lua_state_agent;
-    let module_accessor = sv_system::battle_object_module_accessor(lua_state);
-    if MotionModule::is_end(module_accessor) {
-        let owner = BossModule::get_owner(module_accessor);
-        WorkModule::set_int(owner,*SITUATION_KIND_AIR,FIGHTER_MARIO_INSTANCE_WORK_ID_INT_BOSS_SITUATION);
-        PostureModule::update_rot_y_lr(module_accessor);
-        StatusModule::change_status_request(module_accessor,*ITEM_LIOLEUSBOSS_STATUS_KIND_WAIT_AIR,false);
+        StatusModule::change_status_request(module_accessor,*ITEM_GALLEOM_STATUS_KIND_WAIT,false);
     }
     return L2CValue::I32(0)
 }
 
 pub unsafe fn install_turn(item: &mut L2CAgentBase) {
     let turn_coroutine_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(turn_coroutine as *const () as _).get_ptr());
-    item.sv_set_status_func(L2CValue::I32(*ITEM_LIOLEUSBOSS_STATUS_KIND_ATTACK_TAIL),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS_COROUTINE),turn_coroutine_func);
+    item.sv_set_status_func(L2CValue::I32(*ITEM_GALLEOM_STATUS_KIND_TURN),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS_COROUTINE),turn_coroutine_func);
     let turn_status_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(turn_status as *const () as _).get_ptr());
-    item.sv_set_status_func(L2CValue::I32(*ITEM_LIOLEUSBOSS_STATUS_KIND_ATTACK_TAIL),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS),turn_status_func);
-
-    let turn_air_coroutine_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(turn_air_coroutine as *const () as _).get_ptr());
-    item.sv_set_status_func(L2CValue::I32(*ITEM_LIOLEUSBOSS_STATUS_KIND_TURN_AIR),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS_COROUTINE),turn_air_coroutine_func);
-    let turn_air_status_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(turn_air_status as *const () as _).get_ptr());
-    item.sv_set_status_func(L2CValue::I32(*ITEM_LIOLEUSBOSS_STATUS_KIND_TURN_AIR),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS),turn_air_status_func);
+    item.sv_set_status_func(L2CValue::I32(*ITEM_GALLEOM_STATUS_KIND_TURN),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS),turn_status_func);
 }
