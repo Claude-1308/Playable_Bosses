3e5a27e99f3c56e557159ad5aeecb866c4c1cc8b
ClaudevonRiegan <63413745+ClaudevonRiegan@users.noreply.github.com>
2022-09-21 12:38:24 +0530

    Update specials.rs

--- src/galleom/specials.rs
+++ src/galleom/specials.rs
@@ -20,11 +20,42 @@ pub unsafe fn galleom_missile_fix(fighter: &mut L2CFighterCommon) {
     let module_accessor = sv_system::battle_object_module_accessor(lua_state);
     let boss_id = WorkModule::get_int64(module_accessor,FIGHTER_MARIO_INSTANCE_WORK_ID_INT_BOSS_ID) as u32;
     let boss_boma = sv_battle_object::module_accessor(boss_id);
-    if StatusModule::status_kind(module_accessor) == *ITEM_GALLEOM_STATUS_KIND_MISSILE {
-        if MotionModule::rate(module_accessor) <= 1.0 {
-            MotionModule::set_rate(module_accessor,2.0);
+    if StatusModule::status_kind(boss_boma) == *ITEM_GALLEOM_STATUS_KIND_MISSILE {
+        if MotionModule::rate(boss_boma) <= 1.0 {
+            MotionModule::set_rate(boss_boma,2.0);
         }
     }
+}
+
+pub unsafe fn galleom_large_jump_fix(fighter: &mut L2CFighterCommon) {
+    let lua_state = fighter.lua_state_agent;
+    let module_accessor = sv_system::battle_object_module_accessor(lua_state);
+    let boss_id = WorkModule::get_int64(module_accessor,FIGHTER_MARIO_INSTANCE_WORK_ID_INT_BOSS_ID) as u32;
+    let boss_boma = sv_battle_object::module_accessor(boss_id);
+    if StatusModule::status_kind(boss_boma) == *ITEM_GALLEOM_STATUS_KIND_LARGE_JUMP_MAIN {
+        if MotionModule::frame(boss_boma) <= (some_no) {
+            let add_pos = Vector2f{x: ControlModule::get_stick_x(module_accessor), y: 0.0};
+            PostureModule::add_pos_2d(boss_boma,&add_pos);
+        }
+    }
+}
+
+pub unsafe fn galleom_jump_land_coroutine(item: &mut L2CAgentBase) -> L2CValue {
+    let lua_state = item.lua_state_agent;
+    let module_accessor = sv_system::battle_object_module_accessor(lua_state);
+    MotionModule::change_motion(module_accessor,Hash40::new("jump_land"),0.0,1.5,false,0.0,false,false);
+    boss_private::main_energy_from_param(lua_state,ItemKind(*ITEM_KIND_GALLEOM),Hash40::new("energy_param_large_jump_landing"),0.0);
+    boss_private::sub1_energy_from_param_inherit_all(lua_state,ItemKind(*ITEM_KIND_GALLEOM),Hash40::new("energy_param_large_jump_landing_brake"));
+    return L2CValue::I32(0)
+}
+
+pub unsafe fn galleom_jump_land_status(item: &mut L2CAgentBase) -> L2CValue {
+    let lua_state = item.lua_state_agent;
+    let module_accessor = sv_system::battle_object_module_accessor(lua_state);
+    if MotionModule::is_end(module_accessor) {
+        StatusModule::change_status_request(module_accessor,*ITEM_GALLEOM_STATUS_KIND_WAIT,false);
+    }
+    return L2CValue::I32(0)
 }
 
 pub unsafe fn galleom_lariat_loop_coroutine(item: &mut L2CAgentBase) -> L2CValue {
@@ -53,48 +84,17 @@ pub unsafe fn galleom_lariat_loop_status(item: &mut L2CAgentBase) -> L2CValue {
     return L2CValue::I32(0)
 }
 
-pub unsafe fn galleom_hammer_coroutine(item: &mut L2CAgentBase) -> L2CValue {
+pub unsafe fn galleom_lariat_coroutine(item: &mut L2CAgentBase) -> L2CValue {
     let lua_state = item.lua_state_agent;
     let module_accessor = sv_system::battle_object_module_accessor(lua_state);
-    MotionModule::change_motion(module_accessor,Hash40::new("hammer_knuckle"),0.0,2.0,false,0.0,false,false);
-    boss_private::main_energy_from_param(lua_state,ItemKind(*ITEM_KIND_GALLEOM),Hash40::new("energy_param_hammer_knuckle"),0.0);
+    MotionModule::change_motion(module_accessor,Hash40::new("lariat"),0.0,1.0,false,0.0,false,false);
+    boss_private::main_energy_from_param(lua_state,ItemKind(*ITEM_KIND_GALLEOM),Hash40::new("energy_param_double_lariat"),0.0);
     return L2CValue::I32(0)
 }
 
-pub unsafe fn galleom_hammer_status(item: &mut L2CAgentBase) -> L2CValue {
+pub unsafe fn galleom_lariat_status(item: &mut L2CAgentBase) -> L2CValue {
     let lua_state = item.lua_state_agent;
     let module_accessor = sv_system::battle_object_module_accessor(lua_state);
-    if MotionModule::frame(module_accessor) > 85.0
-    && MotionModule::frame(module_accessor) < 101.0 {
-        MotionModule::set_rate(module_accessor,1.0);
-    }
-    else {
-        MotionModule::set_rate(module_accessor,2.0);
-    }
-    if MotionModule::is_end(module_accessor) {
-        StatusModule::change_status_request(module_accessor,*ITEM_GALLEOM_STATUS_KIND_WAIT,false);
-    }
-    return L2CValue::I32(0)
-}
-
-pub unsafe fn galleom_foot_crush_coroutine(item: &mut L2CAgentBase) -> L2CValue {
-    let lua_state = item.lua_state_agent;
-    let module_accessor = sv_system::battle_object_module_accessor(lua_state);
-    MotionModule::change_motion(module_accessor,Hash40::new("foot_crush"),0.0,1.5,false,0.0,false,false);
-    boss_private::main_energy_from_param(lua_state,ItemKind(*ITEM_KIND_GALLEOM),Hash40::new("energy_param_foot_crush"),0.0);
-    return L2CValue::I32(0)
-}
-
-pub unsafe fn galleom_foot_crush_status(item: &mut L2CAgentBase) -> L2CValue {
-    let lua_state = item.lua_state_agent;
-    let module_accessor = sv_system::battle_object_module_accessor(lua_state);
-    if MotionModule::frame(module_accessor) > 49.0
-    && MotionModule::frame(module_accessor) < 55.0 {
-        MotionModule::set_rate(module_accessor,1.0);
-    }
-    else {
-        MotionModule::set_rate(module_accessor,1.5);
-    }
     if MotionModule::is_end(module_accessor) {
         StatusModule::change_status_request(module_accessor,*ITEM_GALLEOM_STATUS_KIND_WAIT,false);
     }
@@ -102,21 +102,18 @@ pub unsafe fn galleom_foot_crush_status(item: &mut L2CAgentBase) -> L2CValue {
 }
 
 pub unsafe fn install_specials(item: &mut L2CAgentBase) {
-    let lioleusboss_fireball_shot3_end_coroutine_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(lioleusboss_fireball_shot3_end_coroutine as *const () as _).get_ptr());
-    item.sv_set_status_func(L2CValue::I32(*ITEM_LIOLEUSBOSS_STATUS_KIND_ATTACK_FIREBALL3_END),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS_COROUTINE),lioleusboss_fireball_shot3_end_coroutine_func);
-    let lioleusboss_fireball_shot3_end_status_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(lioleusboss_fireball_shot3_end_status as *const () as _).get_ptr());
-    item.sv_set_status_func(L2CValue::I32(*ITEM_LIOLEUSBOSS_STATUS_KIND_ATTACK_FIREBALL3_END),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS),lioleusboss_fireball_shot3_end_status_func);
+    let galleom_jump_land_coroutine_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(galleom_jump_land_coroutine as *const () as _).get_ptr());
+    item.sv_set_status_func(L2CValue::I32(*ITEM_GALLEOM_STATUS_KIND_LARGE_JUMP_LANDING),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS_COROUTINE),galleom_jump_land_coroutine_func);
+    let galleom_jump_land_status_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(galleom_jump_land_status as *const () as _).get_ptr());
+    item.sv_set_status_func(L2CValue::I32(*ITEM_GALLEOM_STATUS_KIND_LARGE_JUMP_LANDING),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS),galleom_jump_land_status_func);
 
-    let lioleusboss_tackle_loop_status_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(lioleusboss_tackle_loop_status as *const () as _).get_ptr());
-    item.sv_set_status_func(L2CValue::I32(*ITEM_LIOLEUSBOSS_STATUS_KIND_ATTACK_TACKLE),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS),lioleusboss_tackle_loop_status_func);
+    let galleom_lariat_loop_coroutine_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(galleom_lariat_loop_coroutine as *const () as _).get_ptr());
+    item.sv_set_status_func(L2CValue::I32(*ITEM_GALLEOM_STATUS_KIND_DOUBLE_LARIAT_CHARGE),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS_COROUTINE),galleom_lariat_loop_coroutine_func);
+    let galleom_lariat_loop_status_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(galleom_lariat_loop_status as *const () as _).get_ptr());
+    item.sv_set_status_func(L2CValue::I32(*ITEM_GALLEOM_STATUS_KIND_DOUBLE_LARIAT_CHARGE),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS),galleom_lariat_loop_status_func);
 
-    let lioleusboss_tackle_end_coroutine_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(lioleusboss_tackle_end_coroutine as *const () as _).get_ptr());
-    item.sv_set_status_func(L2CValue::I32(*ITEM_LIOLEUSBOSS_STATUS_KIND_ATTACK_TACKLE_END),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS_COROUTINE),lioleusboss_tackle_end_coroutine_func);
-    let lioleusboss_tackle_end_status_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(lioleusboss_tackle_end_status as *const () as _).get_ptr());
-    item.sv_set_status_func(L2CValue::I32(*ITEM_LIOLEUSBOSS_STATUS_KIND_ATTACK_TACKLE_END),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS),lioleusboss_tackle_end_status_func);
-
-    let lioleusboss_tackle_turn_coroutine_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(lioleusboss_tackle_turn_coroutine as *const () as _).get_ptr());
-    item.sv_set_status_func(L2CValue::I32(*ITEM_LIOLEUSBOSS_STATUS_KIND_ATTACK_TACKLE_TURN),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS_COROUTINE),lioleusboss_tackle_turn_coroutine_func);
-    let lioleusboss_tackle_turn_status_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(lioleusboss_tackle_turn_status as *const () as _).get_ptr());
-    item.sv_set_status_func(L2CValue::I32(*ITEM_LIOLEUSBOSS_STATUS_KIND_ATTACK_TACKLE_TURN),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS),lioleusboss_tackle_turn_status_func);
+    let galleom_lariat_coroutine_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(galleom_lariat_coroutine as *const () as _).get_ptr());
+    item.sv_set_status_func(L2CValue::I32(*ITEM_GALLEOM_STATUS_KIND_DOUBLE_LARIAT_MAIN),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS_COROUTINE),galleom_lariat_coroutine_func);
+    let galleom_lariat_status_func: &mut skyline::libc::c_void = std::mem::transmute(L2CValue::Ptr(galleom_lariat_status as *const () as _).get_ptr());
+    item.sv_set_status_func(L2CValue::I32(*ITEM_GALLEOM_STATUS_KIND_DOUBLE_LARIAT_MAIN),L2CValue::I32(*ITEM_LUA_SCRIPT_STATUS_FUNC_STATUS),galleom_lariat_status_func);
 }
